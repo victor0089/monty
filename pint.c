4e758625136bf6bb04d4346f4614d3a15e9d665d
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-25 01:48:04 +0300

    Update pint.c

--- pint.c
+++ pint.c
@@ -11,7 +11,6 @@ void pint(stack_t **stack, unsigned int line_number)
 if (*stack == NULL)
 {
 fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-free_stack(*stack);
 exit(EXIT_FAILURE);
 }
 printf("%d\n", (*stack)->n);
