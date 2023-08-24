f2c72a619d4cadd989f3dde15b90a4c4515868af
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-25 01:26:24 +0300

    Update divvv.c

--- divvv.c
+++ divvv.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * div_ The opcode div divides the second top element of the stack by the top element of the stack.
+ * div_ The opcode div divides the second top element of the stack by  of the stack.
  * @stack: Pointerto thestack.
  * @line_number: Linenumber oftheinstruction.
  * Return: void.
