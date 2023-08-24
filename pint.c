55a5ad3ad8d8c17f534d513df752a69051502e88
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-25 01:36:23 +0300

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * The opcode pint prints the value at the top of the stack, followed by a new line.
+ * pint - opcode pint prints the value at the top of the stack a new line.
  * @stack: Pointerto thestack.
  * @line_number: Linenumberof theinstruction.
  * Return: void.
