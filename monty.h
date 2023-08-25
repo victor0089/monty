5f6d39dac6459b42bc7743920f0e388e1f9d6778
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-25 18:55:47 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -37,7 +37,7 @@ typedef struct instruction_s
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-stack_t add_node(stack_t **stack, int n);
+stack_t *add_node(stack_t **stack, int n);
 instruction_t *get_instruction(char *opcode);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
