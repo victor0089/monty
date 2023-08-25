3c74c4d7cee2b963bb6d80ca72f4ff91d972b3a5
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-25 12:48:00 +0300

    Update exc.c

--- exc.c
+++ exc.c
@@ -10,21 +10,21 @@
 int execute(char *line_number, stack_t **stack, unsigned int counter, FILE *file)
 {
 instruction_t opst[] = {
-{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-{"pop", f_pop},
-{"swap", f_swap},
-{"add", f_add},
-{"nop", f_nop},
-{"sub", f_sub},
-{"div", f_div},
-{"mul", f_mul},
-{"mod", f_mod},
-{"pchar", f_pchar},
-{"pstr", f_pstr},
-{"rotl", f_rotl},
-{"rotr", f_rotr},
-{"queue", f_queue},
-{"stack", f_stack},
+{"push", push}, {"pall", pall}, {"pint", pint},
+{"pop", pop},
+{"swap", swap},
+{"add", add},
+{"nop", nop},
+{"sub", sub},
+{"div", div},
+{"mul", mul},
+{"mod", mod},
+{"pchar", pchar},
+{"pstr", pstr},
+{"rotl", rotl},
+{"rotr", rotr},
+{"queue", queue},
+{"stack", stack},
 {NULL, NULL}
 };
 unsigned int i = 0;
