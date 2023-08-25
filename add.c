0cf5c9db1b5b755267ad59ee4ba7e03beb5de5d4
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-25 03:25:05 +0300

    Update add.c

--- add.c
+++ add.c
@@ -8,25 +8,23 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *h;
-	int len = 0, xx;
-
-	h = *stack;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	h = *stack;
-	xx = h->n + h->next->n;
-	h->next->n = xx;
-	*stack = h->next;
-	free(h);
+stack_t *h;
+int len = 0, xx;
+h = *stack;
+while (h)
+{
+h = h->next;
+len++;
+}
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+free_stack(*stack);
+exit(EXIT_FAILURE);
+}
+h = *stack;
+xx = h->n + h->next->n;
+h->next->n = xx;
+*stack = h->next;
+free(h);
+}
